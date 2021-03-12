2e36e45e9f1f77d2341cc766636d79b76bccfae3
Giova <giovanotti1906@hotmail.com>
2021-03-11 20:16:43 -0500

    Created state for Participant

--- lib/bloc/participants/participants_state.dart
+++ lib/bloc/participants/participants_state.dart
@@ -1,1 +1,30 @@
-class ParticipantsState {}
+import 'package:spotiamigos/models/participant.dart';
+import 'package:spotiamigos/services/participant_service.dart';
+
+class ParticipantsState {
+  ParticipantService _participantService = ParticipantService();
+
+  addParticipant(Participant participant) async {
+    await _participantService.save(participant);
+  }
+
+  deleteParticipant(int id) async {
+    await _participantService.delete(id);
+  }
+
+  deleteAllParticipants() async {
+    await _participantService.deleteAll();
+  }
+
+  updateParticipant(Participant participant) async {
+    await _participantService.update(participant);
+  }
+
+  Future<Participant> getParticipant(int id) async {
+    return await _participantService.findById(id);
+  }
+
+  Future<List<Participant>> getParticipants() async {
+    return await _participantService.findAll();
+  }
+}
